cbed2976e496d3329690577ef8cfebc3c0008f70
T1P3X <ulisminton@gmail.com>
2022-05-23 22:55:53 +0200

    Add Player Modal Added

--- repository/badmintonTracker/src/app/tab2/tab2.page.ts
+++ repository/badmintonTracker/src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController, IonRouterOutlet } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public routerOutlet: IonRouterOutlet, private actionSheetCtrl: ActionSheetController) {}
 
+  async canDismiss() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Are you sure you want to discard your changes?',
+      buttons: [
+        {
+          text: 'Discard Changes',
+          role: 'destructive'
+        },
+        {
+          text: 'Keep Editing',
+          role: 'cancel'
+        }
+      ]
+    });
+    
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    
+    if (role === 'destructive') {
+      return true;
+    }
+
+    return false;
+  }
 }

f16c90f26b398bcb33ec8e7bc467ada56ceffa9d
T1P3X <ulisminton@gmail.com>
2022-06-05 19:54:06 +0200

    Alert on Log Out

--- repository/badmintonTracker/src/app/tabs/tabs.page.ts
+++ repository/badmintonTracker/src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { AuthenticationService } from '../shared/authentication-service';
 
 @Component({
@@ -8,6 +9,29 @@ import { AuthenticationService } from '../shared/authentication-service';
 })
 export class TabsPage {
 
-  constructor(public authenticationService: AuthenticationService) {}
+  constructor(public authenticationService: AuthenticationService, private alertController: AlertController) {}
+
+  async logout(){
+
+    const alert = await this.alertController.create({
+      header: 'Do you want to log out from this account?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          id: 'cancel-button'
+        }, {
+          text: "Confirm",
+          id: 'confirm-button',
+          handler: () => {
+            this.authenticationService.SignOut();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
